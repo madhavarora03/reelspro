c6f873787428663ea012602a04f9bd7cd3ced8bc
Madhav Arora <eragon.cube@outlook.com>
2025-02-01 22:45:50 +0530

    Create User and Video models

---
+++ constants.ts
@@ -0,0 +1,4 @@
+export const VIDEO_DIMENSIONS = {
+  height: 1920,
+  width: 1080,
+} as const;
---
+++ models/Video.ts
@@ -0,0 +1,37 @@
+import { Schema, models, model, Document } from "mongoose";
+import { VIDEO_DIMENSIONS } from "@/constants";
+
+export interface IVideo extends Document {
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  controls?: boolean;
+  transformation?: {
+    height: number;
+    width: number;
+    quality?: number;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const videoSchema = new Schema<IVideo>(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    videoUrl: { type: String, required: true },
+    thumbnailUrl: { type: String, required: true },
+    controls: { type: Boolean, default: true },
+    transformation: {
+      height: { type: Number, default: VIDEO_DIMENSIONS.height },
+      width: { type: Number, default: VIDEO_DIMENSIONS.width },
+      quality: { type: Number, min: 1, max: 100 },
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = models.Video || model<IVideo>("Video", videoSchema);
+
+export default Video;
